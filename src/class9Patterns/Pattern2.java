bc7d743436df216f6bb01726eae9a0dcdf05523a
Roshni <roshnikc12212@gmail.com>
2026-02-24 21:33:24 +0530

    commit

---
+++ src/class9Patterns/Pattern2.java
@@ -0,0 +1,30 @@
+package class9Patterns;
+
+public class Pattern2 {
+public static void main(String[] args) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+		System.out.print(i +"" + ""+j);	//(i +"" + ""+j+" ")no need of secont line+
+		System.out.print(" ");
+		}
+		System.out.println();
+	}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
