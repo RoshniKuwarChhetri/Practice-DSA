087318d79fa06359376d9cda0909ae814646b40e
Roshni <roshnikc12212@gmail.com>
2026-05-21 23:16:52 +0530

    pattern2

---
+++ src/practice7/patterns/LeftTriangle.java
@@ -0,0 +1,17 @@
+package practice7.patterns;
+
+public class LeftTriangle {
+	public static void main(String[] args) {
+		int n=6;
+		for(int i=1;i<=n-1;i++) {
+			for(int k=0;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+		    System.out.println();
+		}
+	}
+
+}
