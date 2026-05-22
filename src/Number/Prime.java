224f25ef39500c8719f5036ab4ed499bb8044693
Roshni <roshnikc12212@gmail.com>
2026-05-22 20:23:56 +0530

    commit2

---
+++ src/Number/Prime.java
@@ -0,0 +1,22 @@
+package Number;
+
+public class Prime {
+public static void main(String[] args) {
+	int n1=1;
+	int n2=100;
+	
+	for(int i=n1;i<=n2;i++) {
+		int count=0;
+		for(int j=1;j<=i;j++) {
+			if(i%j==0) {
+				count++;
+			}
+			
+		}
+		if(count==2) {
+			System.out.print(i+",");
+		}
+		
+	}
+}
+}
