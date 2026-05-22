c1318c84906064e625319119168c0d6e37f54286
Roshni <roshnikc12212@gmail.com>
2026-05-22 21:07:35 +0530

    commit

---
+++ src/practiceString/ReverseEntireString.java
@@ -0,0 +1,16 @@
+package practiceString;
+
+public class ReverseEntireString {
+public static void main(String[] args) {
+	System.out.println(Rev("hello Roshni"));
+}
+ public static String Rev(String s) {
+	 String word="";
+	 String rev="";
+	 char arr []=s.toCharArray();
+	 for(int i=arr.length-1;i>=0;i--) {
+		 rev=rev+arr[i];
+	 }
+	 return rev;
+ }
+}
