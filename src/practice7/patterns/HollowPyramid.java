22930f0a6f9d6a6c9485580ef8620a5c14c2bf2c
Roshni <roshnikc12212@gmail.com>
2026-05-23 19:39:28 +0530

    pattern

--- src/practice7/patterns/HollowPyramid.java
+++ src/practice7/patterns/HollowPyramid.java
@@ -3,16 +3,15 @@ package practice7.patterns;
 public class HollowPyramid {
  public static void main(String[] args) {
 	int n=5;
-	for(int i=1;i<=n;i++) {
-		for(int k=n;k>=1;k--) {
+	for(int i =0;i<=n;i++) {
+		for(int k=1;k<=n-i;k++) {
 			System.out.print(" ");
 		}
-		for(int j=1;i<=i;j++) {
-			if(i==j || i==n||j==1) {
-              System.out.print("*");
-			}
-			else {
-				System.out.print(" ");
+		for(int j=1;j<=i;j++) {
+			if(i==j || j==1 || i==n) {
+				System.out.print("* ");
+			}else {
+				System.out.print("  ");
 			}
 		}
 		System.out.println();
