d0cc8979d67f40baa1311c5893b6b16b50711923
Roshni <roshnikc12212@gmail.com>
2026-05-24 10:31:36 +0530

    commit

---
+++ src/practice7/patterns/RevFibannacci.java
@@ -0,0 +1,18 @@
+package practice7.patterns;
+
+public class RevFibannacci {
+public static void main(String[] args) {
+	int n=4;
+	int n1=34;
+	int n2=21;
+	for(int i=n;i>=1;i--) {
+		for(int j=1;j<=i;j++) {
+			System.out.print(n1+" ");
+			int n3=n1-n2;
+			n1=n2;
+			n2=n3;
+		}
+		System.out.println();
+	}
+}
+}
