4cd365d020d9299a7edb52817ae877f7ca3eead6
Roshni <roshnikc12212@gmail.com>
2026-05-24 08:06:31 +0530

    commit

---
+++ src/practice7/patterns/FloydTriangle.java
@@ -0,0 +1,18 @@
+package practice7.patterns;
+
+public class FloydTriangle {
+public static void main(String[] args) {
+	int n=4;
+	int x=1;
+	for (int i =1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			
+				System.out.print(x+" ");
+				x++;
+			
+		}
+		System.err.println();
+	}
+}
+
+}
