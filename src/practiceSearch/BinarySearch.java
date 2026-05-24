dab8094579fe6b339e8cb059aa0ff9ae63889934
Roshni <roshnikc12212@gmail.com>
2026-05-24 17:52:01 +0530

    commit

---
+++ src/practiceSearch/BinarySearch.java
@@ -0,0 +1,26 @@
+package practiceSearch;
+
+public class BinarySearch {
+public static void main(String[] args) {
+	int arr[]= {1,2,3,4,5};
+	System.out.println(p(arr,4));
+}
+public static int p(int arr[], int target) {
+	int start=0;
+	int end=arr.length-1;
+	
+	while(start<=end) {
+		int mid=(start+end)/2;
+		if(arr[mid]==target) {
+			return mid;
+		}else if(arr[mid]>target) {
+			end=mid-1;
+		}else {
+			start=mid+1;
+		}
+	}
+	
+	return -1;
+	
+}
+}
