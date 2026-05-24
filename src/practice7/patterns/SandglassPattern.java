709b53a1088d8d1fc5b846eeb90a230a30f57656
Roshni <roshnikc12212@gmail.com>
2026-05-24 09:52:19 +0530

    commit

---
+++ src/practice7/patterns/SandglassPattern.java
@@ -0,0 +1,35 @@
+package practice7.patterns;
+
+public class SandglassPattern {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<n;i++) {
+			for(int k=1;k<=i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*(n-i)-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<n;i++) {
+			for(int k=1;k<=n-i-1;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//*********
+// *******
+//  *****
+//   ***
+//    *
+//   ***
+//  *****
+// *******
+//*********
