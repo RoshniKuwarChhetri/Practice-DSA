c1a3122ecfeb90faa4570810e5be541b6689d390
Roshni <roshnikc12212@gmail.com>
2026-05-24 10:13:31 +0530

    commit

---
+++ src/practice7/patterns/AlternateNumberPyramid.java
@@ -0,0 +1,26 @@
+package practice7.patterns;
+
+public class AlternateNumberPyramid {
+public static void main(String[] args) {
+	int n=5;
+	int x=1;
+	for(int i=1;i<=n;i++) {
+		for(int k=1;k<=n-i;k++) {
+			System.out.print(" ");
+		}
+		
+			if(i%2!=0) {
+				for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+				}
+			}
+				else {
+					for(int j=i;j>=1;j--) {
+						System.out.print(j+" ");
+						}
+				}
+			System.out.println();
+		}
+	}
+}
+
