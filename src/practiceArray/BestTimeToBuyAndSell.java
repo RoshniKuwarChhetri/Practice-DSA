e1c8ca096778d48a7e7fe2c117633b4267709b62
Roshni <roshnikc12212@gmail.com>
2026-05-27 08:17:28 +0530

    commit

--- src/practiceArray/BestTimeToBuyAndSell.java
+++ src/practiceArray/BestTimeToBuyAndSell.java
@@ -2,4 +2,26 @@ package practiceArray;
 
 public class BestTimeToBuyAndSell {
 
+public static int p(int price []) {
+	if(price==null || price.length==0 ) {
+		return 0;
+	}
+	int minPrice=price[0];
+	int maxProfit=0;
+	for(int i=1;i<price.length;i++) {
+		int profit=price[i]-minPrice;
+		
+		if(profit>maxProfit) {
+			maxProfit=profit;
+		}
+		
+		if(price[i] <= minPrice) {
+			minPrice=price[i];
+		}
+		
+		
+	}
+	return maxProfit;
+}
+
 }
