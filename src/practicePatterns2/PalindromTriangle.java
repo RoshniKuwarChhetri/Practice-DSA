94e56eea18a0cd3af6e5c64d6e1e9e4ebe3a0ebb
Roshni <roshnikc12212@gmail.com>
2026-06-08 00:37:11 +0530

    palindrome triangle

---
+++ src/practicePatterns2/PalindromTriangle.java
@@ -0,0 +1,16 @@
+package practicePatterns2;
+
+public class PalindromTriangle {
+public static void main(String[] args) {
+	int n=4;
+	for(int i=0;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print(j);
+		}
+		for(int j=i-1;j>=1;j--) {
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+}
+}
