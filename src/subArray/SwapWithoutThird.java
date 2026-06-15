fc35cac82ebd4590e753105691261f575aebcb3e
Roshni <roshnikc12212@gmail.com>
2026-06-15 17:02:35 +0530

    commit

---
+++ src/subArray/SwapWithoutThird.java
@@ -0,0 +1,13 @@
+package subArray;
+
+public class SwapWithoutThird {
+public static void main(String[] args) {
+	int a=1;
+	int b=2;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
