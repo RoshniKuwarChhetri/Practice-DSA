ec8014c7e21737243b68d43ce3729e6516645f3a
Roshni <roshnikc12212@gmail.com>
2026-06-15 16:36:04 +0530

    commit

---
+++ src/subArray/ArmstrongNum.java
@@ -0,0 +1,29 @@
+package subArray;
+
+public class ArmstrongNum {
+	public static void main(String[] args) {
+		System.out.println(p(153));
+	}
+	public static boolean p(int n) {
+		
+		int count=0;
+		int sum=0;
+		int n1=n;
+		
+		while(n>0) {
+			int digit=n%10;
+			count++;
+			n=n/10;
+		}
+		int n2=n1;
+		while(n1>0) {
+			int lastDigit=n1%10;
+			sum=(int) (sum+Math.pow(lastDigit,count));
+			n1=n1/10;
+		}
+		
+		return n2==sum;
+		
+	}
+
+}
