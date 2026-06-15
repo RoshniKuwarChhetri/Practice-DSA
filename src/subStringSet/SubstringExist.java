5c26f9f0ba8fd7a3e6e4dfb69d517eebc09ab0ae
Roshni <roshnikc12212@gmail.com>
2026-06-15 20:31:44 +0530

    commit

---
+++ src/subStringSet/SubstringExist.java
@@ -0,0 +1,20 @@
+package subStringSet;
+
+public class SubstringExist {
+	public static void main(String[] args) {
+		System.out.println(p("roshni"));
+	}
+public static boolean p(String s) {
+	
+	String sub;
+	for(int i=0;i<s.length();i++) {
+		for(int j=i+1;j<=s.length();j++) {
+			if(s.substring(i,j).contains("9")){
+			return true;
+		}
+	}
+	
+}
+	return false;
+}
+}
