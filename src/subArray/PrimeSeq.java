715997b1260d867a877ac64dae33497969550069
Roshni <roshnikc12212@gmail.com>
2026-06-15 16:51:58 +0530

    commit

---
+++ src/subArray/PrimeSeq.java
@@ -0,0 +1,24 @@
+package subArray;
+
+public class PrimeSeq {
+	public static void main(String[] args) {
+		p(1,100);
+	}
+	public static void p(int start,int end ) {
+	
+		for(int i=start;i<=end;i++) {		
+			int count=0;
+			for(int j=2;j<=i;j++) {
+				if(i%j==0) {
+					count++;
+				}
+				
+				
+			}
+			if(count==1) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
