a54deca5b7b62e7cf2e4ca6cc0458df53523d479
Roshni <roshnikc12212@gmail.com>
2026-06-15 16:59:24 +0530

    commit

---
+++ src/subArray/PrimeCheck.java
@@ -0,0 +1,17 @@
+package subArray;
+
+public class PrimeCheck {
+public static void main(String[] args) {
+	
+	System.out.println(p(8));
+}
+public static boolean p(int n) {
+	int count=0;
+	for(int i=2;i<=n;i++) {
+		if(n%i==0) {
+			count++;
+		}
+	}
+	return count==1;
+}
+}
