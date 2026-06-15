03b5c441edf63a8ccd85f20b794d24cf719c157f
Roshni <roshnikc12212@gmail.com>
2026-06-15 21:07:25 +0530

    commit

---
+++ src/subStringSet/OccuranceofExist.java
@@ -0,0 +1,20 @@
+package subStringSet;
+
+public class OccuranceofExist {
+	public static void main(String[] args) {
+		System.out.println(p("roshni","ro"));
+	}
+
+	public static int p(String s,String sub) {
+		
+		int count=0;
+		for(int i=0;i<s.length();i++) {
+			for(int j=i+1;j<=s.length();j++) {
+				if(s.substring(i,j).equals(sub)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+}
--- src/subStringSet/SubstringExist.java
+++ src/subStringSet/SubstringExist.java
@@ -9,7 +9,7 @@ public static boolean p(String s) {
 	String sub;
 	for(int i=0;i<s.length();i++) {
 		for(int j=i+1;j<=s.length();j++) {
-			if(s.substring(i,j).contains("9")){
+			if(s.substring(i,j).contains("ro")){
 			return true;
 		}
 	}
