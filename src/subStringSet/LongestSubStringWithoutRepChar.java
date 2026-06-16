8bfde8f586034e62a40c9977087337e4ccdc50cc
Roshni <roshnikc12212@gmail.com>
2026-06-16 16:40:11 +0530

    commit

---
+++ src/subStringSet/LongestSubStringWithoutRepChar.java
@@ -0,0 +1,26 @@
+package subStringSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubStringWithoutRepChar {
+	public static void main(String[] args) {
+		System.out.println(p("abcabcbb"));
+	}
+public static int p(String s) {
+	Set<Character> set=new HashSet<>();
+	int max=0;
+	int left=0;
+	for(int right=0;right<s.length();right++) {
+		while(set.contains(s.charAt(right))) {
+			set.remove(s.charAt(left));
+			left++;
+		}
+		set.add(s.charAt(right));
+		max=Math.max(max, right-left+1);
+	}
+	return max;
+}
+	
+	
+}
