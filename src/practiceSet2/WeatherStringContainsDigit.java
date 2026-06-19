821dc43352241ff8f4f23f5318b41f8d96e29946
Roshni <roshnikc12212@gmail.com>
2026-06-19 16:36:11 +0530

    commit

---
+++ src/practiceSet2/WeatherStringContainsDigit.java
@@ -0,0 +1,15 @@
+package practiceSet2;
+
+public class WeatherStringContainsDigit {
+public static void main(String[] args) {
+	System.out.println(p("Roshni))0"));
+}
+public static boolean p(String s) {
+	for(char c: s.toCharArray()) {
+		if(c>='0' && c<='9') {
+			return true;
+		}
+	}
+	return false;
+}
+}
