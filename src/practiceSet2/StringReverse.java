5385a700e3944ce28d5a04ab8673ae319234b158
Roshni <roshnikc12212@gmail.com>
2026-06-19 10:19:24 +0530

    commit

---
+++ src/practiceSet2/StringReverse.java
@@ -0,0 +1,22 @@
+package practiceSet2;
+
+public class StringReverse {
+public static void main(String[] args) {
+	System.out.println(p("roshni"));
+}
+public static String p(String s) {
+	char arr[]=s.toCharArray();
+	int i=0;
+	int j = arr.length-1;
+	while(i<=j) {
+		char temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]= temp;
+		i++;
+		j--;
+		
+	}
+	String rev=new String(arr);
+	return rev;
+}
+}
