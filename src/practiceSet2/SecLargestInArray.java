7ff5b0abade90b5e35eee4e7725054f5440e6cee
Roshni <roshnikc12212@gmail.com>
2026-06-19 19:02:25 +0530

    commit

---
+++ src/practiceSet2/SecLargestInArray.java
@@ -0,0 +1,22 @@
+package practiceSet2;
+
+public class SecLargestInArray {
+ public static void main(String[] args) {
+	 int arr[]= {1,2,4,6,-9,-8,0};
+	System.out.println(p(arr));
+}
+ public static int p(int arr[]) {
+	 int max=Integer.MIN_VALUE;
+	 int sMax=0;
+	 for(int i=0;i<arr.length;i++) {
+		 if(arr[i]>max) {
+			 sMax=max;
+			 max=arr[i];
+			 
+		 }else if(arr[i]<= max && arr[i]>=sMax){
+			 sMax=arr[i];
+		 }
+	 }
+	 return sMax;
+ }
+}
