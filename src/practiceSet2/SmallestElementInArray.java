17e6d3254169d4d811cb53b141eab831c19b4e79
Roshni <roshnikc12212@gmail.com>
2026-06-19 16:53:06 +0530

    commit

---
+++ src/practiceSet2/SmallestElementInArray.java
@@ -0,0 +1,17 @@
+package practiceSet2;
+
+public class SmallestElementInArray {
+public static void main(String[] args) {
+	int arr[] ={1,3,4,0,-5};
+	System.out.println(p(arr));
+}
+public static int p(int arr[]) {
+	int min=Integer.MAX_VALUE;
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]<=min) {
+			min=arr[i];
+		}
+	}
+	return min;
+}
+}
