abcabb728e3cdad482562e0f94c795cba215b709
Roshni <roshnikc12212@gmail.com>
2026-06-19 16:21:46 +0530

    commit

---
+++ src/practiceSet2/ReverseWordInSentence.java
@@ -0,0 +1,23 @@
+package practiceSet2;
+
+public class ReverseWordInSentence {
+public static void main(String[] args) {
+	System.out.println(p("i am a developer"));
+}
+public static String p(String s) {
+	String rev="";
+	String word="";
+	for(char c: s.toCharArray() ) {
+		if(c!=' ') {
+			word=word+c;
+		}
+		else {
+			rev=word+" "+rev;
+			word="";
+		}
+	}
+	rev=word+" "+rev;
+	return rev.trim();
+	
+}
+}
