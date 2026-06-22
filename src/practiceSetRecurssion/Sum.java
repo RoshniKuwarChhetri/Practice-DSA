b903e06f16794feaad483997dd7bab472fe58c7d
Roshni <roshnikc12212@gmail.com>
2026-06-22 10:33:14 +0530

    commit

---
+++ src/practiceSetRecurssion/Sum.java
@@ -0,0 +1,14 @@
+package practiceSetRecurssion;
+
+public class Sum {
+	public static void main(String[] args) {
+		System.out.println(p(99));
+	}
+public static int p(int n) {
+	if(n==0) {
+	return n;	
+	}
+	
+	return n%10 + p(n/10);
+}
+}
