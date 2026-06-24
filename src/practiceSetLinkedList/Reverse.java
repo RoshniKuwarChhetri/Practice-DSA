dc4e8fd64254b75454cac7521efa47f13c6aa41d
Roshni <roshnikc12212@gmail.com>
2026-06-24 22:33:57 +0530

    commit

--- src/practiceSetLinkedList/Reverse.java
+++ src/practiceSetLinkedList/Reverse.java
@@ -2,4 +2,26 @@ package practiceSetLinkedList;
 
 public class Reverse {
 
+class Node{
+	int val;
+	Node next;
+	Node(int val){
+		this.val=val;
+	}
+}
+
+public static Node P(Node head) {
+	Node prev=null;
+	Node curr=head;
+	
+	while(curr!=null) {
+		
+		Node next=curr.next;
+		curr.next=prev;
+		
+		prev=curr;
+		curr=next;
+	}
+	return prev;		
+}
 }
