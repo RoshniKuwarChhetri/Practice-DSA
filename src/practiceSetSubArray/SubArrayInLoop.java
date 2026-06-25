3666646ad062239625e7d4717095824eccda4a22
Roshni <roshnikc12212@gmail.com>
2026-06-25 17:58:56 +0530

    commit

---
+++ src/practiceSetSubArray/SubArrayInLoop.java
@@ -0,0 +1,20 @@
+package practiceSetSubArray;
+
+public class SubArrayInLoop {
+
+    public static void main(String[] args) {
+    	int arr[]= {1,2,3};
+    p(arr);
+    }
+    public static void p(int arr[]) {
+    	
+    	for(int i=0;i<arr.length;i++) {
+    		for(int j=i;j<arr.length;j++) {
+    			for(int k=i;k<=j;k++) {
+    				System.out.print(arr[k]);
+    			}
+    			System.out.println();
+    		}
+    	}
+    }
+}
