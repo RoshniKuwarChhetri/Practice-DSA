5b9056dd96db3277f1caad9bd899df3f6b5c5767
Roshni <roshnikc12212@gmail.com>
2026-07-18 23:16:48 +0530

    commit

--- src/practiceSetString/WordFrequency.java
+++ src/practiceSetString/WordFrequency.java
@@ -1,5 +1,17 @@
 package practiceSetString;
 
-public class WordFrequency {
+import java.util.HashMap;
 
+public class WordFrequency {
+public static void main(String[] args) {
+	
+}
+public static HashMap<String,Integer> p(String s) {
+	String word="";	
+  HashMap<Character,Integer> map=new HashMap<>();
+  for(int i =0;i<s.length();i++){
+	 char c=s.charAt(i);
+	 
+  }
+}
 }
