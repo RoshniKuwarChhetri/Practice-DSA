e66b601af226f913a0fd2e4e666472da1abbad20
Roshni <roshnikc12212@gmail.com>
2026-07-20 22:22:43 +0530

    commit

--- src/class2assign/Youngest.java
+++ src/class2assign/Youngest.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Youngest {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter raghav's age");
-		int raghav_age=sc.nextInt();
+		System.out.println("enter rani's age");
+		int rani_age=sc.nextInt();
 		System.out.println("enter raghu's age");
 		int Raghu_age=sc.nextInt();
 		System.out.println("enter raju's age");
 		int raju_age=sc.nextInt();
-		System.out.println(p3(raghav_age,Raghu_age,raju_age));
+		System.out.println(p3(rani_age,Raghu_age,raju_age));
 	}
 
-	public static int p3(int raghav_age, int Raghu_age, int raju_age) {
+	public static int p3(int rani_age, int Raghu_age, int raju_age) {
 		int youngest=0;
-		if(raghav_age< Raghu_age && Raghu_age < raju_age) {
-			youngest=raghav_age;
+		if(rani_age< Raghu_age && Raghu_age < raju_age) {
+			youngest=rani_age;
 		}
-		else if(Raghu_age<raju_age && raju_age<raghav_age) {
+		else if(Raghu_age<raju_age && raju_age<rani_age) {
 			youngest=Raghu_age;
 		}
 		else {
